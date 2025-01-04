85d6f627cad51107cb2dd43df816cf02b5399810
Vladimir Elisavetski <yvova88@yahoo.com>
2025-01-04 14:27:00 -0700

    configuring tailwind

---
+++ src/data.js
@@ -0,0 +1,74 @@
+export const DATA = {
+    aboutMe: "AWS Certified | Full-stack developer skilled in seamless user experiences, efficient backend systems, & cloud integration. Proficient in React & Node.js. Delivering solutions that drive growth & elevate platforms.",
+    contact: {
+        email: "vladimir.elisavetsky@gmail.com",
+        phone: "206-555-1234",
+        location: "Calgary",
+        postCode: "T2P 0L6"
+    },
+    experience: [
+        {
+            company: "Amazon",
+            title: "Software Development Engineer",
+            startDate: "June 2019",
+            endDate: null,
+            length: "2 yrs 3 mos",
+            description: "Developed scalable, high-performing, and secure software solutions for Amazon's global platform. Leveraged AWS services to build and deploy cloud-based applications. Collaborated with cross-functional teams to deliver high-quality software solutions."
+        },
+        {
+            company: "Microsoft",
+            title: "Software Engineer Intern",
+            startDate: "May 2018",
+            endDate: "August 2018", 
+            length: "4 mos",
+            description: "Developed and maintained software solutions for Microsoft's Azure platform. Worked with a team of engineers to build and deploy cloud-based applications. Collaborated with cross-functional teams to deliver high-quality software solutions."
+        }
+    ],
+    education: [
+        {
+            school: "University of Washington",
+            degree: "Bachelor of Science in Computer Science",
+            startDate: "September 2015",
+            endDate: "June 2019",
+            description: "Studied computer science and software engineering. Developed software applications and systems. Graduated with a 3.9 GPA."
+        },
+        {
+            school: "Elevation",
+            degree: "Certificate in Full-Stack Web Development",
+            startDate: "September 2015",
+            endDate: "June 2019",
+            description: "Studied computer science and software engineering. Developed software applications and systems. Graduated with a 3.9 GPA."
+        },
+        Certifications = [
+            {
+                name: "AWS Certified Developer",
+                date: "June 2020",
+                description: "Certified by Amazon Web Services as a Developer. Demonstrated proficiency in developing, deploying, and maintaining applications on AWS.",
+                organization: "Amazon Web Services",
+                logo: {awsCertifiedDeveloper}
+            },
+            {
+                name: "AWS Certified Solutions Architect",
+                date: "June 2020",
+                description: "Certified by Amazon Web Services as a Solutions Architect. Demonstrated proficiency in designing, deploying, and managing applications on AWS.",
+                organization: "uDemy",
+                logo: {awsUdemy}
+            }
+        ]
+    ],
+    skills: [
+        {
+
+        }
+    ],
+    links: [
+        {
+            name: "LinkedIn",
+            url: "https://www.linkedin.com/in/username"
+        },
+        {
+            name: "GitHub",
+            url: "",
+        }
+    ]
+}
